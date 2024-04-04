7bcd359f16a66e27cb43f9516fd8e0297c1460a1
MasatoKihara25 <1539.kihara@gmail.com>
2024-04-05 00:06:26 +0900

    エラーハンドリング実装のためBookController、BookMapper修正。BookNotFoundException、BookService、CustomExceptionHandler追加

--- src/main/java/com/example/bookservice/BookController.java
+++ src/main/java/com/example/bookservice/BookController.java
@@ -1,6 +1,7 @@
 package com.example.bookservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -8,15 +9,21 @@ import java.util.List;
 @RestController
 public class BookController {
 
-    private BookMapper bookMapper;
+    private BookService bookService;
 
-    public BookController(BookMapper bookMapper) {
-        this.bookMapper = bookMapper;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
     }
 
     @GetMapping("/books")
     public List<Book> findByBook(BookSearchRequest request) {
-        List<Book> book = bookMapper.findBook(request.getStartsWith(), request.getEndsWith(), request.getContains());
+        List<Book> book = bookService.findBook(request.getStartsWith(), request.getEndsWith(), request.getContains());
         return book;
     }
+
+    @GetMapping("/books/{id}")
+    public Book findBook(@PathVariable("id") int id) {
+        return bookService.findBook(id);
+    }
+
 }
